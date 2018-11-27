8d9e331b7aa1c3c49133530b821172e84ec776c1
BD Softech <1nasir2khan3@gmail.com>
2018-11-27 12:47:26 +0600

    Updates

---
+++ precache-manifest.2be3c3045779919598eccf00f73e24df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ddf7340b5506ecbf3a5",
+    "url": "/react-examples/static/js/runtime~main.4ddf7340.js"
+  },
+  {
+    "revision": "81cd045311119b4be9c3",
+    "url": "/react-examples/static/js/main.81cd0453.chunk.js"
+  },
+  {
+    "revision": "8dd967122435f5f5520c",
+    "url": "/react-examples/static/js/1.8dd96712.chunk.js"
+  },
+  {
+    "revision": "81cd045311119b4be9c3",
+    "url": "/react-examples/static/css/main.0ddc2bea.chunk.css"
+  },
+  {
+    "revision": "8dd967122435f5f5520c",
+    "url": "/react-examples/static/css/1.0f73421c.chunk.css"
+  },
+  {
+    "revision": "56799c4f893a1bd415397e75981cb7c4",
+    "url": "/react-examples/index.html"
+  }
+];
